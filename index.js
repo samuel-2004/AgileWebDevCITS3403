d72ecff2ba5523c99ff618cb34ca1bd872d113ae
samuel-2004 <105411500+samuel-2004@users.noreply.github.com>
2024-04-15 15:04:18 +0800

    Added displayItem method to class Item
    When the displayItem method is called, js will add the HTML code to index.html. 
    This includes displaying the image, its name, its suburb and who is offering the item.

--- index.js
+++ index.js
@@ -9,4 +9,30 @@ class Item {
         this.suburb = suburb;
         this.imageref = ref;
     }
+    displayItem() {
+        // Create a div element
+        var elemDiv = document.createElement("div");
+        elemDiv.id = "someid";
+        elemDiv.classList.add("anItem");
+        elemDiv.classList.add("col-lg-2");
+        elemDiv.classList.add("col-md-3");
+        elemDiv.classList.add("col-sm-5");
+        elemDiv.classList.add("col-xs-6");
+
+        // Create an image element
+        const img = document.createElement("img");
+        img.src = this.imageref;
+
+        // Create paragraph elements for name, suburb, and who
+        const text = document.createElement("p");
+        text.textContent = `Name: ${this.name}\nSuburb: ${this.suburb}\nWho: ${this.who}`
+
+        // Append image and paragraphs to the div
+        elemDiv.appendChild(img);
+        elemDiv.appendChild(text);
+        
+        // Append the div to the document body
+        //document.body.appendChild(elemDiv);
+        $('#mainContent').append(elemDiv);
+    }
 }
