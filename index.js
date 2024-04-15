3f22dee47f548c6d848398a7e23047f78f4a7e65
samuel-2004 <samuel@lewis.id.au>
2024-04-15 16:35:38 +0800

    Properly included jquery
    Actually learnt how to include jquery and use it in a webpage.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.7.1.min.js';
-document.getElementsByTagName('head')[0].appendChild(script);
-
 class Item {
     constructor(name, who, suburb, ref) {
         this.name = name;
