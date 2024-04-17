455099d3c8eb52999c03861990f88be0d8a51b92
samuel-2004 <samuel@lewis.id.au>
2024-04-17 16:49:10 +0800

    Adds sorting to index.js
    Implemented sort by newest/oldest.
    Still todo: Implement sorting by distance and vendor rating

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 class Item {
-    constructor(name, who, suburb, ref) {
+    constructor(name, who, suburb, ref, timestamp) {
         this.name = name;
         this.who = who;
         this.suburb = suburb;
         this.imageref = ref;
         this.timestamp = timestamp;
+    }
+    isItem() {
+        return true;
+    }
+    getTimeStamp() {
+        return this.timestamp;
     }
     displayItem() {
         // Create a div element
@@ -21,7 +27,7 @@ class Item {
         if (this.imageref != "") {
             img.src = this.imageref;
         } else {
-            img.src = "book.jpg";
+            img.src = "book.jpg"; //default
         }
 
         // Create paragraph elements for name, suburb, and who
@@ -38,16 +44,61 @@ class Item {
     }
 }
 
+function sortItems(itemsList, order) {
+    //Check itemsList is a valid list
+    if (!Array.isArray(itemsList)) {
+        return false;
+    }
+    $.each(itemsList, function( index, item ) {
+        if (!item.isItem()) {
+            return false;
+        }
+    });
+
+    //Sort the list by the order given
+    order = order.toLowerCase();
+    switch (order) {
+        case "newest":
+            itemsList = itemsList.sort(function(a, b) {
+                return a.getTimeStamp() - b.getTimeStamp();
+            });
+            break;
+        case "oldest":
+            itemsList = itemsList.sort(function(a, b) {
+                return b.getTimeStamp() - a.getTimeStamp();
+            });
+            break;
+        case "closest":
+            // to implement
+            break;
+        case "vendor+rating":
+            // to implement
+            break;
+        default: // Order is not an acceptable value
+            return false;
+    }
+
+    return true;
+}
+
 window.onload = (event) => {
     //The following code is temporary and used for debugging
     //Create a generic list of items
     //this will need to change to GET information from server
     const itemsList = [];
     for (var i = 0; i < 20; i++) {
-        console.log(i);
-        console.log(li[i]);
         itemsList.push(new Item(li[i].name, li[i].who, li[i].suburb, li[i].imageref, li[i].timestamp));
     }
+
+    //Sort the list
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+    console.log(page);
+    var order = "newest"
+    if (page == "index.html") {
+        order = "newest";
+    }
+    sortItems(itemsList, order);
 
     //If home page: display all
     //If search page: only display items that fit the search criteria
