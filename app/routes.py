9292310efd4aa977f6ffa05e7bfaf6c60e537195
samuel-2004 <samuel@lewis.id.au>
2024-04-22 14:27:23 +0800

    routes.py now provides active link
    Navbar now displays which link is active. I.e. if the home page is currently being viewed, the navbar will reflect this.

--- app/routes.py
+++ app/routes.py
@@ -8,11 +8,11 @@ from os.path import join as os_join, dirname as os_dirname, exists as os_pathexi
 @flaskApp.route('/', methods=['GET'])
 def index():
     items = loads('[{"nID":87241,"name":"Rice Cooker","who":"John Smith","suburb":"Downtown","imageref":"","timestamp":1679776345},{"nID":52379,"name":"Smartphone","who":"Emily Johnson","suburb":"Midtown","imageref":"","timestamp":1679818345},{"nID":10294,"name":"Laptop","who":"Michael Brown","suburb":"Uptown","imageref":"","timestamp":1679762345},{"nID":40957,"name":"Bicycle","who":"Sarah Davis","suburb":"Eastside","imageref":"","timestamp":1679790345},{"nID":78526,"name":"Television","who":"DavnID Wilson","suburb":"Westside","imageref":"","timestamp":1679720345},{"nID":63081,"name":"Coffee Maker","who":"Jessica Martinez","suburb":"Downtown","imageref":"","timestamp":1679804345},{"nID":21789,"name":"Headphones","who":"Christopher Lee","suburb":"Midtown","imageref":"","timestamp":1679748345},{"nID":95873,"name":"Backpack","who":"Jennifer Thompson","suburb":"Uptown","imageref":"","timestamp":1679734345},{"nID":37402,"name":"Digital Camera","who":"Daniel Garcia","suburb":"Eastside","imageref":"","timestamp":1679822345},{"nID":69023,"name":"Printer","who":"Olivia Hernandez","suburb":"Westside","imageref":"","timestamp":1679706345},{"nID":18396,"name":"Blender","who":"William Rodriguez","suburb":"Downtown","imageref":"","timestamp":1679692345},{"nID":54127,"name":"Smart Watch","who":"Ava Wilson","suburb":"Midtown","imageref":"","timestamp":1679678345},{"nID":76258,"name":"Gaming Console","who":"Ethan Moore","suburb":"Uptown","imageref":"","timestamp":1679664345},{"nID":89501,"name":"Tablet","who":"Sophia Anderson","suburb":"Eastside","imageref":"","timestamp":1679650345},{"nID":32095,"name":"Microwave Oven","who":"James Taylor","suburb":"Westside","imageref":"","timestamp":1679636345},{"nID":61478,"name":"Fitness Tracker","who":"Mia Thomas","suburb":"Downtown","imageref":"","timestamp":1679622345},{"nID":94602,"name":"Portable Speaker","who":"Benjamin White","suburb":"Midtown","imageref":"","timestamp":1679608345},{"nID":25814,"name":"Vacuum Cleaner","who":"Isabella Martinez","suburb":"Uptown","imageref":"","timestamp":1679594345},{"nID":70183,"name":"Kitchen Scale","who":"Alexander Johnson","suburb":"Eastside","imageref":"","timestamp":1679580345},{"nID":18347,"name":"Digital Watch","who":"Charlotte Brown","suburb":"Westside","imageref":"","timestamp":1679566345}]')
-    return render_template('index.html', items=items, defaultimage='book.jpg')
+    return render_template('index.html', items=items, defaultimage='book.jpg', active_link='/')
 
 @flaskApp.route('/advancedsearch')
 def advancedSearch():
-    return render_template('advancedsearch.html')
+    return render_template('advancedsearch.html', active_link='/advancedsearch')
 
 @flaskApp.route('/search', methods=['GET'])
 def search():
@@ -28,7 +28,7 @@ def item(itemID):
 
 @flaskApp.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('login.html', active_link='/login')
 
 @flaskApp.route('/upload')
 def upload():
