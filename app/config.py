fa86d842df0e1c04c0de04fe4b030bc913b8f75b
Johnny-2003 <johnson02che@gmail.com>
2024-04-26 11:08:56 +0800

    Updated config.py and created venv and deliverables

--- app/config.py
+++ app/config.py
@@ -2,7 +2,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    # ...
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLACLHEMY_TRACK_MODIFICATIONS = False
