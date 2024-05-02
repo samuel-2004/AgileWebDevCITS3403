534cc75886fa800e506d5462d3d41af930ff04cf
Joel Brooker <joeltbrooker4@gmail.com>
2024-05-02 17:24:28 +0800

    Commented out imports causing errors

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+#from config import Config
+#from flask_sqlalchemy import SQLAlchemy
+#from flask_migrate import Migrate
 
 flaskApp = Flask(__name__)
 #flaskApp.config.from_object(Config)
