410f7d076049581cf0ed4483025ff11efd185a08
Johnny-2003 <johnson02che@gmail.com>
2024-05-08 15:31:48 +0800

    Update config.py for testing

--- config.py
+++ config.py
@@ -5,3 +5,15 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLACLHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess' # SET MANUALLY, DEFAULT ONLY FOR TESTING
+
+    # Testing Purposes
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory"
+    SQLACLHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
+
+class DeploymentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'test.db')
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory"
+    TESTING = True;
