adeacb8da15cf0957d26fbbe04f315c8dc7db123
samuel-2004 <samuel@lewis.id.au>
2024-05-13 01:28:07 +0800

    Expanding navbar on focus

--- app/static/items.js
+++ app/static/items.js
@@ -21,3 +21,50 @@ function openItemPage(event, nID) {
     }
 }
 
+// When user starts typing in the search box in the navbar
+function typingStarted() {
+    document.getElementById('navbarsearch').classList.add("w-75");
+}
+
+// When user stops typing in the search box in the navbar
+function doneTyping() {
+    document.getElementById('navbarsearch').classList.remove("w-75");
+}
+
+// When the search box is focused
+function onFocus() {
+    document.getElementById('navbarsearch').classList.add("w-75");
+}
+
+function expandSearchBar() {
+    const searchInput = document.getElementById('navbarsearchbox');
+
+    let typingTimer;
+    const doneTypingInterval = 3000;
+
+
+    // Event listener for input event (typing)
+    searchInput.addEventListener('input', function() {
+        clearTimeout(typingTimer);
+        typingStarted(); // Perform action when user starts typing
+        typingTimer = setTimeout(doneTyping, doneTypingInterval);
+    });
+
+    // Event listener for blur event (unfocused)
+    searchInput.addEventListener('blur', function() {
+        clearTimeout(typingTimer);
+        doneTyping(); // Perform action when the search box is unfocused
+        typingTimer = setTimeout(doneTyping, doneTypingInterval);
+    });
+
+    // Event listener for focus event
+    searchInput.addEventListener('focus', function() {
+        clearTimeout(typingTimer);
+        onFocus();
+        typingTimer = setTimeout(doneTyping, doneTypingInterval);
+    });
+}
+
+$( document ).ready(function() {
+    expandSearchBar();
+});
