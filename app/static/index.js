eef129f797a3b4c54a48aad450817865c3e0135c
Matthew Pfleger <161227771+MattPfleger@users.noreply.github.com>
2024-05-13 11:49:20 +0800

    API call can now run while window loads

--- app/static/index.js
+++ app/static/index.js
@@ -4,7 +4,6 @@ function getQuote() {
     xhttp.onload = function() {
       let result = JSON.parse(this.responseText);
       loadQuote(result[0])
-      console.log(result[0]);
     }
     xhttp.open("GET", "https://api.quotable.io/quotes/random?tags=inspirational&maxLength=100", true);
     xhttp.setRequestHeader("Content-type", "application/json");
@@ -12,8 +11,10 @@ function getQuote() {
   }
 
 function loadQuote(quote){
+  while (document.readyState != "complete"){} //Ensures window is loaded
   quoteText = quote.content+"<br>- "+quote.author
   quoteElement = $("<h3></h3>").html(quoteText);
   $("#content").prepend(quoteElement);
 }
-window.onload = () => {getQuote()}
+
+getQuote() // Can run before window.onload for faster retrieval 
