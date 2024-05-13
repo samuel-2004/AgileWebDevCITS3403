b734e72903c2286f9645cd9c2a23168a084fab51
samuel-2004 <samuel@lewis.id.au>
2024-05-13 15:26:57 +0800

    add a limit to the number of returned posts

--- app/models.py
+++ app/models.py
@@ -70,7 +70,7 @@ class Post(db.Model):
     def __repr__(self) -> str:
         return f'<Post {self.id} {self.item_name} {self.desc} {self.timestamp}>'
 
-def get_posts(q="", md=None, order="new"):
+def get_posts(q="", md=None, order="new", lim=100):
     query = db.session.query(
             Post.id,Post.post_type,Post.item_name,Post.timestamp,User.username,Address.city,Address.postcode,Image.src
         ).join(User, Post.user_id==User.id).\
@@ -99,6 +99,7 @@ def get_posts(q="", md=None, order="new"):
         pass
 
     #print("\n\n\n",query,"\n\n\n")
+    query = query.limit(lim)
     posts = db.session.execute(query).fetchall()
     posts = [post._asdict() for post in posts]
     posts = convertPostsTimestamps(posts)
