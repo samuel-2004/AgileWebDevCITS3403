e9edc091a31525c3c8f6c728bec57f0405a04223
samuel-2004 <samuel@lewis.id.au>
2024-05-13 13:03:32 +0800

    site.sql indented properly

--- app/site.sql
+++ app/site.sql
@@ -8,35 +8,35 @@ CREATE TABLE Users (
 );
 
 CREATE TABLE Addresses (
-  addressID int PRIMARY KEY,
-  streetName varchar(255),
-  streetNumber varchar(10),
-  postcode varchar(10),
-  city varchar(32),
-  state varchar(32)
-  country varchar(255)
+    addressID int PRIMARY KEY,
+    streetName varchar(255),
+    streetNumber varchar(10),
+    postcode varchar(10),
+    city varchar(32),
+    state varchar(32)
+    country varchar(255)
 );
 
 CREATE TABLE Posts (
-  postId int PRIMARY KEY,
-  userId_FK int NOT NULL,
-  itemName varchar(255),
-  itemDescription varchar(Max)
-  image1_FK int,
-  image2_FK int,
-  image3_FK int,
-  image4_FK int,
-  image5_FK int
+    postId int PRIMARY KEY,
+    userId_FK int NOT NULL,
+    itemName varchar(255),
+    itemDescription varchar(Max)
+    image1_FK int,
+    image2_FK int,
+    image3_FK int,
+    image4_FK int,
+    image5_FK int
 );
 
 CREATE TABLE Images (
-  imageId int PRIMARY KEY,
-  imageData BLOB
+    imageId int PRIMARY KEY,
+    imageData BLOB
 );
 
 CREATE TABLE Replies (
-  replyId int PRIMARY KEY,
-  userId_FK int NOT NULL,
-  postId_FK int NOT NULL,
-  msg varchar(Max)
+    replyId int PRIMARY KEY,
+    userId_FK int NOT NULL,
+    postId_FK int NOT NULL,
+    msg varchar(Max)
 );
