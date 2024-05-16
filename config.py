5a75c89b8bccf06798a7cc870d5645f2c9b28d40
Johnny-2003 <146691133+Johnny-2003@users.noreply.github.com>
2024-05-16 15:01:46 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLACLHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess' # SET MANUALLY, DEFAULT ONLY FOR TESTING
