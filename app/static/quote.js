823f3beba9bd41f352bb3c6c10c011cf4e6724d2
Matthew Pfleger <matthew.pfleger@icloud.com>
2024-05-16 16:27:31 +0800

    Updated quote.js, README and upload.html

--- app/static/quote.js
+++ app/static/quote.js
@@ -1,21 +1,20 @@
-// Adapted from https://www.w3schools.com/js/js_ajax_intro.asp
+//Adapted from https://www.w3schools.com/js/js_ajax_intro.asp
 function getQuote() {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
         let result = JSON.parse(this.responseText);
-        loadQuote(result[0]);
+        loadQuote(result[0])
+        console.log(result[0])
     }
     xhttp.open("GET", "https://api.quotable.io/quotes/random?tags=inspirational&maxLength=100", true);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send();
-
 }
 
-async function loadQuote(quote){
-    while (document.readyState != "complete"){} // Ensures window is loaded
-    let quoteText = quote.content+"<br>- "+quote.author;
+function loadQuote(quote){
+    let quoteText = quote.content+"<br>- "+quote.author
     let quoteElement = $("<h3></h3>").html(quoteText);
     $("#content").prepend(quoteElement);
 }
 
-getQuote(); // Can run before window.onload for faster retrieval
+window.onload = () => {getQuote();};
